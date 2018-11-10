3dcd0c4480418ca94f936187b77750981bef243d
Souheil CHELFOUH <trollfot@gmail.com>
2018-11-11 00:43:55 +0100

    Fixed is_upgrade. Provided a couple of tests.

--- testing/test_headers.py
+++ testing/test_headers.py
@@ -19,6 +19,22 @@ def test_repeated_continuation_header():
     hdr = HttpStream(stream).headers()
     assert hdr['X-Test'] == 'foo bar, baz qux'
 
+def test_upgrade_header():
+    stream = io.BytesIO(b'GET /test HTTP/1.1\r\nConnection: keep-alive, upgrade\r\nUpgrade: websocket\r\nSec-WebSocket-Key: hojIvDoHedBucveephosh8==\r\nSec-WebSocket-Version: 13\r\n\r\n')
+    hdr = HttpStream(stream)
+    hdr.headers()
+    assert hdr.parser.is_upgrade() == 1
+
+    stream = io.BytesIO(b'GET /test HTTP/1.1\r\nConnection: upgrade\r\nUpgrade: websocket\r\nSec-WebSocket-Key: hojIvDoHedBucveephosh8==\r\nSec-WebSocket-Version: 13\r\n\r\n')
+    hdr = HttpStream(stream)
+    hdr.headers()
+    assert hdr.parser.is_upgrade() == 1
+
+    stream = io.BytesIO(b'GET /test HTTP/1.1\r\nConnection: shenanigans\r\n\r\n')
+    hdr = HttpStream(stream)
+    hdr.headers()
+    assert hdr.parser.is_upgrade() == 0
+
 def test_continuation_header_py():
     stream = io.BytesIO(b'GET /test HTTP/1.1\r\nX-Test: foo\r\n bar\r\n\r\n')
     hdr = HttpStream(stream, parser_class=PyHttpParser).headers()
